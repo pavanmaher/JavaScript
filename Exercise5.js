cae3d5c93c79bb451ff00fa9701bf689873b9b2e
pavan <pavan@gmail.com>
2019-06-24 19:14:30 +0530

    javascript using classes completed

--- Exercise5.js
+++ Exercise5.js
@@ -1,52 +1,31 @@
-let selectionBox1=document.getElementById("first");
-let selectionBox2=document.getElementById("second");
-
-let button1=document.getElementById("add");
-let button2=document.getElementById("remove");
-
-button1.addEventListener("click",add);
-button2.addEventListener("click",remove);
-
-function add()
-{
-
-	let array=[];
-	for(let i=0;i<selectionBox1.options.length;i++)
-	{
-		if(selectionBox1.options[i].selected==true)
-		{
-			selectionBox1.options[i].selected=false;
-			array.push(selectionBox1.options[i]);	
-		}
+class SelectionBox {
+	constructor(id1, id2) {
+		this.countries = document.getElementById(id1);
+		this.button = document.getElementById(id2);
 	}
-	for(let i=0;i<array.length;i++)
-	{
-		selectionBox1.removeChild(array[i]);
-	}
-	for(let i=0;i<array.length;i++)
-	{
-		selectionBox2.appendChild(array[i]);
+	addListener(box) {
+		this.button.addEventListener(
+			"click",
+			function() {
+				let array = [];
+				for (let i = 0; i < this.countries.options.length; i++) {
+					if (this.countries.options[i].selected == true) {
+						this.countries.options[i].selected = false;
+						array.push(this.countries.options[i]);
+					}
+				}
+				for (let i = 0; i < array.length; i++) {
+					this.countries.removeChild(array[i]);
+				}
+				for (let i = 0; i < array.length; i++) {
+					box.countries.appendChild(array[i]);
+				}
+			}.bind(this)
+		);
 	}
 }
+let box1 = new SelectionBox("first", "add");
+let box2 = new SelectionBox("second", "remove");
 
-function remove()
-{
-
-	let array=[];
-	for(let i=0;i<selectionBox2.options.length;i++)
-	{
-		if(selectionBox2.options[i].selected==true)
-		{
-			selectionBox2.options[i].selected=false;
-			array.push(selectionBox2.options[i]);	
-		}
-	}
-	for(let i=0;i<array.length;i++)
-	{
-		selectionBox2.removeChild(array[i]);
-	}
-	for(let i=0;i<array.length;i++)
-	{
-		selectionBox1.appendChild(array[i]);
-	}
-}
+box1.addListener(box2);
+box2.addListener(box1);
